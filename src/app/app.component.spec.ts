670c57e5b8e4f0bdb6ec631097d50096e10a71b1
TinaSchickle <tina.schickle@gmx.de>
2022-01-21 11:01:31 +0100

    change folder

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,16 +5,12 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it('should create the app..', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
@@ -30,6 +26,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('cgm-example-application app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'cgm-example-application app is running!'
+    );
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,58 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cgm-example-application';
+  title = 'Coding Challenge';
+  num1!: number;
+  num2!: number;
+  num3!: number;
+  num4!: number;
+  num5!: number;
+  maxValue!: number;
+  minValue!: number;
+  msg!: string;
+  e!: any;
+  dred = 'The lowest number will be displayed in red';
+  dgreen = 'The highest number will be displayed in green';
+
+  clickEvent(e: { preventDefault: () => void }) {
+    this.msg = 'Please wait 10 seconds, currently sorting numbers';
+
+    setTimeout(() => {
+      this.msg = '';
+      //alle eingaben in einem Array speichern
+      const total = [this.num1, this.num2, this.num3, this.num4, this.num5];
+      
+      //Min&Max berechnen
+      const calcMin = Math.min(...total);
+      const calcMax = Math.max(...total);
+
+      //Min&Max-Stelle im total-array herausfinden
+      const minIndex = total.indexOf(calcMin);
+      const maxIndex = total.indexOf(calcMax);
+
+      //kleinste Zahl rot einfärben
+      if (minIndex === 0)
+        document.getElementById(`number1`)!.style.backgroundColor = 'red';
+      else
+        document.getElementById(
+          `number${minIndex + 1}`
+        )!.style.backgroundColor = 'red';
+
+      //größte Zahl grün einfärben
+      if (maxIndex === 0)
+        document.getElementById(`number1`)!.style.backgroundColor = 'green';
+      else {
+        document.getElementById(
+          `number${maxIndex + 1}`
+        )!.style.backgroundColor = 'green';
+      }
+
+      //text anpassen
+      this.dred = 'The lowest number is displayed in red';
+      this.dgreen = 'The highest number is displayed in red';
+    }, 10000);
+  }
 }
